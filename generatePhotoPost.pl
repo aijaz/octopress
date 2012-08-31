a6dc576e6e8c29fc427d85b6352a880176eec3e5
Aijaz A. Ansari <aijaz@enoor.com>
2012-08-30 23:52:12 -0500

    Done with v1 of photos

--- generatePhotoPost.pl
+++ generatePhotoPost.pl
@@ -8,6 +8,8 @@ use Image::ExifTool qw(:Public);
 use File::Basename;
 use File::Path qw (make_path);
 
+my $creator = "Aijaz Ansari";
+
 my $file;
 my $image;
 my $title;
@@ -29,21 +31,91 @@ $fileTitle    =~ s/\s+/-/g;
 $fileTitle    =~ s/^\-//;
 $fileTitle    =~ s/\-$//;
 
-my $dir = sprintf("source/posts/$d/%02d", $year, $mon);
-
+my $dir = sprintf("source/_posts/%d/%02d", $year, $mon);
+print "Directory: $dir\n";
 
 if (! -e $dir) {
+    print "Making directory\n";
     make_path($dir);
 }
 elsif (! -d $dir) {
     die "ERROR: $dir is a file.  It needs to be a directory.";
 }
 
-$fileName = sprintf("$dir/%d-%02d-%02d-%s.markdown", $year, $mon, $mday, $fileTitle);
+my $fileName = sprintf("$dir/%d-%02d-%02d-%s.markdown", $year, $mon, $mday, $fileTitle);
+
+my $exif = ImageInfo($image);
+
+if (-e $fileName) {
+    die "$fileName already exists - Will not overwrite an existing file";
+}
+
+my $postDate = sprintf("%d-%02d-%02d %02d:%02d", $year, $mon, $mday, $hour, $min);
+
+my $imageUrl = $image;
+$imageUrl =~ s/.*source//;
+
+my $tags = "";
+if (@tags) {
+    $tags = join("\n",
+                 "tags:",
+                 (map { "- $_" } @tags),
+                 ""
+        );
+}
+
+open (F, ">$fileName") || die "Cannot open $fileName for writing";
+print F join("\n",
+             "---",
+             "layout: photo",
+             "title: \"$title\"",
+             "date: $postDate",
+             "comments: false",
+             "categories:",
+             "- Photos",
+             "author: Aijaz Ansari",
+             "${tags}image: $imageUrl",
+             "",
+    );
+
+my %hash = ();
+
+#print Dumper (keys %$exif);
+
+if ($exif->{ISO}) { $hash{iso} = $exif->{ISO}; }
+if ($exif->{Aperture}) { $hash{aperture} = $exif->{Aperture}; }
+if ($exif->{"ShutterSpeed"}) { $hash{shutterSpeed} = $exif->{"ShutterSpeed"}; }
+if ($exif->{"FocalLength"}) { $hash{focalLength} = $exif->{"FocalLength"}; }
+if ($exif->{"ScaleFactor35efl"}) { $hash{scaleFactor} = $exif->{"ScaleFactor35efl"}; }
+if ($exif->{"Flash"}) { $hash{flash} = $exif->{"Flash"}; }
+if ($exif->{"ExposureCompensation"}) { $hash{expComp} = $exif->{"ExposureCompensation"}; }
+if ($exif->{"Model"}) { $hash{camera} = $exif->{"Model"}; }
+if ($exif->{"LensID"}) { $hash{lens} = $exif->{"LensID"}; }
+elsif ($exif->{"LensInfo"}) { $hash{lens} = $exif->{"LensInfo"}; }
+elsif ($exif->{"LensModel"}) { $hash{lens} = $exif->{"LensModel"}; }
+elsif ($exif->{"Lens"}) { $hash{lens} = $exif->{"Lens"}; }
+if ($exif->{"Creator"}) { $hash{creator} = $exif->{"Creator"}; }
+if ($exif->{"DateTimeOriginal"}) { $hash{dateTaken} = $exif->{"DateTimeOriginal"}; }
+elsif ($exif->{"CreateDate"}) { $hash{dateTaken} = $exif->{"CreateDate"}; }
+elsif ($exif->{"DateCreated"}) { $hash{dateTaken} = $exif->{"DateCreated"}; }
+if ($exif->{"Copyright"}) { $hash{copyright} = $exif->{"Copyright"}; }
+elsif ($exif->{"CopyrightNotice"}) { $hash{copyright} = $exif->{"CopyrightNotice"}; }
+
+$hash{creator} = $creator unless $hash{creator};
+$hash{lens} = '"'.$hash{lens}.'"';
+
+my $license = qq^
+<a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/deed.en_US"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-nd/3.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/StillImage" property="dct:title" rel="dct:type">$title</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="{{ site.root }}/$imageUrl" property="cc:attributionName" rel="cc:attributionURL">$hash{creator}</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/deed.en_US">Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License</a>.
+^;
+
+$hash{license} = qq[<a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/deed.en_US"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-nd/3.0/88x31.png" /></a>];
+
+foreach my $k (keys (%hash) ) {
+    print  F "$k: $hash{$k}\n";
+}
+print F "---\n";
 
 
 
-
-
-
+close F;
 
--- source/tags/CloudGate/index.markdown
+++ source/tags/CloudGate/index.markdown
@@ -1,6 +1,6 @@
 ---
 layout: page
-title: Tag&#58; Chicago
+title: Tag&#58; CloudGate
 footer: false
 ---
 
@@ -13,15 +13,6 @@ footer: false
 <footer>
 <span class="categories">posted in 
 <a href='/categories/photos/'>Photos</a></span>
-</footer>
-</article>
-
-<article>
-<h1><a href="/2012/01/20/stopping-by-the-streets/index.html">Stopping by the Streets on a Snowy Evening</a></h1>
-<time datetime="2012-01-20T00:00:00-06:00" pubdate><span class='month'>Jan</span> <span class='day'>20</span> <span class='year'>2012</span></time>
-<footer>
-<span class="categories">posted in 
-<a href='/categories/photography/'>Photography</a></span>
 </footer>
 </article>
 </div>
